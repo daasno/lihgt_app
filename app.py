685d7a2d152007c728e9599161a108399dd20174
ISLAH-Hamza <hamza2028islah@gmail.com>
2023-02-20 21:23:57 +0000

    sommeModefications

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pickle
+from pathlib import Path
+import streamlit as st
+import yaml
+from yaml import SafeLoader
+import main
+import import_data
+
+
+st.set_page_config(page_title="Data analysis", page_icon=":bar_chart:", layout="wide")
+page=st.sidebar.selectbox('selectioner la page: ',['main','import'])
+st.sidebar.markdown('---')
+if page=='main':
+    main.creatpage()
+if page=='import':
+    import_data.creatpage()
+    st.sidebar.markdown('Remarque: importer les corrects données pour chaque selection')
+st.sidebar.markdown('---')
--- generate_password.py
+++
@@ -1,4 +0,0 @@
-import streamlit_authenticator as stauth
-
-hashed_passwords = stauth.Hasher(['XXXXX']).generate()
-print(hashed_passwords)
